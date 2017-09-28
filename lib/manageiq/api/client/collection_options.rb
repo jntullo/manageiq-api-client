39d93721319c34b2418518e0e8bdf4969f3860ff
Jillian Tullo <jtullo@redhat.com>
2017-09-28 08:26:36 -0400

    allow the parameters used in the metric rollups api

--- lib/manageiq/api/client/collection_options.rb
+++ lib/manageiq/api/client/collection_options.rb
@@ -6,10 +6,16 @@ module ManageIQ
         attr_reader :virtual_attributes
         attr_reader :relationships
         attr_reader :data
+        attr_reader :resource_type
+        attr_reader :capture_interval
+        attr_reader :start_date
+        attr_reader :end_date
+        attr_reader :resource_ids
+
 
         def initialize(options = {})
-          @attributes, @virtual_attributes, @relationships, @data =
-            options.values_at("attributes", "virtual_attributes", "relationships", "data")
+          @attributes, @virtual_attributes, @relationships, @data, @resource_type, @capture_interval, @start_date, @end_date, @resource_ids =
+            options.values_at("attributes", "virtual_attributes", "relationships", "data", "resource_type", "capture_interval", "start_date", "end_date", "resource_ids")
         end
       end
     end
